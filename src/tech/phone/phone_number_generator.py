f36f985aeafdc89ff18126b997c1ab186a7de7da
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2023-03-20 12:27:18 +0100

    Use random generator checks to add complexity and realism to phone numbers

--- src/tech/phone/phone_number_generator.py
+++ src/tech/phone/phone_number_generator.py
@@ -51,13 +51,31 @@ class PhoneNumberGenerator(RandomGenerator):
         digits,
     )
 
+    checks = (
+        "no_three_following_digits",
+        "no_more_than_four_same_digits",
+        "no_scale_patterns",
+    )
+
     @classmethod
-    def is_allowed(cls, code: str) -> bool:
+    def check_no_three_following_digits(cls, code: str) -> bool:
         """Return whether this code is allowed (only check the end)."""
         allowed = True
-        code = code.replace("-", "")
         if len(code) >= 3:
             last = code[-1]
             allowed = not code.endswith(last * 3)
 
         return allowed
+
+    @classmethod
+    def check_no_more_than_four_same_digits(cls, code: str) -> bool:
+        """check that the same digit is present no more than 4 times."""
+        return all(code.count(char) < 4 for char in code if char.isdigit())
+
+    @classmethod
+    def check_no_scale_patterns(cls, code: str) -> bool:
+        """Avoid scale patterns (123, 321...)."""
+        code = code.replace("-", "")
+        forbidden = [f"{i}{i + 1}{i + 2}" for i in range(1, 8)]
+        forbidden += [f"{i}{i - 1}{i - 2}" for i in range(3, 10)]
+        return all(scale not in code for scale in forbidden)
